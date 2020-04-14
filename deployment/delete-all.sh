a39b9f3129e126502953cc92454700c8c6868ed7
Harald Uebele <uebele@de.ibm.com>
2020-04-14 11:48:03 +0200

    fix

--- deployment/delete-all.sh
+++ deployment/delete-all.sh
@@ -7,4 +7,4 @@ kubectl delete -f ../istio/mtls.yaml --ignore-not-found
 kubectl delete -f articles.yaml --ignore-not-found
 kubectl delete -f authors.yaml --ignore-not-found
 kubectl delete -f web-api.yaml --ignore-not-found
-kubectl delete -f istio-ingress.yaml --ignore-not-found
+kubectl delete authorizationpolicy articlesaccess --ignore-not-found
